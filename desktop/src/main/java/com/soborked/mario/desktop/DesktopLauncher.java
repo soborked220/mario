e6ca294fff60494817c842c483302919fa02ca70
ed sobork <soborked220@gmail.com>
2018-09-08 12:11:47 -0500

    Project setup courtesy of libgdx setup

---
+++ desktop/src/main/java/com/soborked/mario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.soborked.mario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.soborked.mario.MarioGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioGame(), config);
+	}
+}
